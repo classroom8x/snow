aceaaa194ac761fcf0a44e40d0695ebf56e3c4e4
classroom8x <315629480+classroom8x@users.noreply.github.com>
2026-08-31 20:18:15 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Apne GitHub ka username aur repo name daalein
+  site: 'https://classroom8x.github.io',
+  base: '/snow/',
+});
